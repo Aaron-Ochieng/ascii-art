689521499194e54a27289dced9fb518f2e381740
aaochieng <ochiengaoron@gmail.com>
2024-04-19 09:21:32 +0300

    update

--- main.go
+++ main.go
@@ -6,15 +6,34 @@ import (
 	"os"
 )
 
-func main() {
+type Range struct {
+	From int
+	To   int
+}
 
+func AsciiMap(arr []int) map[int]Range {
+	res := map[int]Range{}
+	for i := range arr {
+		if i+1 < len(arr) {
+			res[32+i] = Range{From: arr[i+1] - 8, To: arr[i+1] - 1}
+		}
+		// else if i == len(arr)-1 {
+		// 	res[32+1] = Range{From: arr[i-1] - 8, To: arr[i+1] - 1}
+		// }
+	}
+	return res
+}
+
+func main() {
+	// str := "Boy"
 	// Read the commandline arguments
 	// arg := os.Args[1:]
 
 	// Read a file
-	file, error := os.Open("standard.txt")
-	if error != nil {
-		fmt.Println(error)
+	file, err := os.Open("standard.txt")
+
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -23,6 +42,8 @@ func main() {
 
 	emptyLines := []int{}
 	countLine := 1
+
+	// For scanning the file to fill the array where there are empty values
 	for scanner.Scan() {
 		line := scanner.Text()
 
@@ -36,10 +57,30 @@ func main() {
 	defer file.Close()
 
 	// Print the arguments
-	res := AsciiMap(emptyLines)
+	linesToRead := AsciiMap(emptyLines)
+	lineNum := 0
 
-	for i, val := range res {
-		fmt.Printf("The ascii values of %d is from : %d to : %d  \n", i, val.From, val.To)
+	// scan the file for specific values based on the string and map values
+	for scanner.Scan() {
+		fmt.Println("called")
+		lineNum++
+		if rangeToread, ok := linesToRead[lineNum]; ok {
+			if lineNum >= rangeToread.From && lineNum <= rangeToread.To {
+				fmt.Println(scanner.Text())
+			}
+		}
 	}
 
+	// for i, val := range res {
+	// 	fmt.Printf("The ascii values of %d is from : %d to : %d  \n", i, val.From, val.To)
+	// }
+	// fmt.Println(emptyLines)
+	// fmt.Println(res)
+
+	// Handling any scanner errors
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(linesToRead)
 }
