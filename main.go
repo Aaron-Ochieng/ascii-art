3f6da384a16f88b08a4c2d695fa44eaf871b776b
ebarsula <ebarsulai@gmail.com>
2024-04-20 10:34:10 +0300

    split

--- main.go
+++ main.go
@@ -4,42 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	// "strings"
 )
 
 func main() {
 
-	// Read the commandline arguments
-	// arg := os.Args[1:]
-
-	// Read a file
 	file, error := os.Open("standard.txt")
 	if error != nil {
 		fmt.Println(error)
 	}
-
+	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	scanner.Split(bufio.ScanLines)
+	
 
-	emptyLines := []int{}
-	countLine := 1
+	countLine := 0
+	var str string
 	for scanner.Scan() {
 		line := scanner.Text()
-
-		if len(line) == 0 {
-			emptyLines = append(emptyLines, countLine)
-		}
 		countLine++
-
+		if countLine >= 1 && countLine <= 856 {
+			str += string(line) + "\n"
+		}
+	
 	}
 
-	defer file.Close()
+	
 
-	// Print the arguments
-	res := AsciiMap(emptyLines)
-
-	for i, val := range res {
-		fmt.Printf("The ascii values of %d is from : %d to : %d  \n", i, val.From, val.To)
-	}
+strArr := strings.Split(str, "\n")
+fmt.Print(len(strArr))
 
 }
