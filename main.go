86bc0d16c6a9ab9d800f1f082a25164d0bb9fb60
ebarsula <ebarsulai@gmail.com>
2024-04-22 12:32:13 +0300

    new line

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	var str_args string
+	var str_arg string
 	if len(args) != 0 {
-		str_args += args[0]
+		str_arg += args[0]
 	}
 	// Open file
-
+	str_args := strings.Split(str_arg, "\\n")
 	f, err := os.ReadFile("standard.txt")
 
 	if err != nil {
@@ -26,22 +26,21 @@ func main() {
 	strArr := strings.Split(string(f), "\n")
 
 	
-	var ascii []string
-	for i := 0; i < 8; i++ {
-		ascii = append(ascii, "")
-	}
+
 
 	for i := 0; i < len(str_args); i++ {
-		start_point := (int(str_args[i])-32)*9 + 1
-		for i := 0; i < len(ascii); i++ {
-			ascii[i] += strArr[start_point+i]
+		for k := 0; k < 8; k++ {
+			for j :=0; j < len(str_args[i]); j++{
+				start_point := (int(str_args[i][j])-32)*9 + 1
+					fmt.Print( strArr[start_point+k])
+			}
+			fmt.Println()
 		}
+	
 
 	}
 	// fmt.Println(str_args)
 
 
-	for i := 0; i < len(ascii); i++ {
-		fmt.Println(string(ascii[i]))
-	}
+	
 }
