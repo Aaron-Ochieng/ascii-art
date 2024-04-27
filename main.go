ea54e6838d16a15048d0337b6a6fd590921c7662
Aaron-Ochieng <ochiengaoron@outlook.com>
2024-04-27 11:29:11 +0300

    changes

--- main.go
+++ main.go
@@ -12,44 +12,58 @@ func errHandler(err error) {
 	}
 }
 
-func stringContain(s string) []string {
-	if strings.Contains(s, "o") {
-		return strings.Split(s, "\r\n")
-	}
-	return strings.Split(s, "\n")
+// generating the starting number on the ascii art file
+func getStartingIndex(ascii int) int {
+	return (ascii-32)*9 + 2
 }
 
-func getIndex(ascii int) int {
-	return (ascii-32)*9 + 1
+func handleBackspace(s string) string {
+	index := strings.Index(s, "\\b") // obtaining the index of the first occurance of \b character
+
+	if index == -1 { // Base case: no "\b" found, return the string as is
+		return s
+	}
+	if index == 0 { // If "\b" is at the beginning, remove it and return the rest of the string
+		return handleBackspace(s[1:])
+	}
+	return handleBackspace(s[:index-1] + s[index+2:]) // Remove the "\b" and the character before it recursively
 }
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . <inputstring>")
+		fmt.Println("Usage: go run . <input string>")
+		return
 	}
-	input := os.Args[1]
-	// fmt.Println(input)
+
+	input := os.Args[1] // user input
 
 	if input == "" {
 		return
 	}
-
+	if input == "\\n" {
+		fmt.Println()
+		return
+	}
+	input = handleBackspace(input)
+	input = strings.ReplaceAll(string(input), "\\t", "   ") // handling the tab sequence
 	file, err := os.ReadFile("standard.txt")
 	errHandler(err)
-	// fmt.Println(len(file))
 
-	fileData := stringContain(string(file))
-	inputData := strings.Split(input, "\\n")
-	for _, word := range inputData {
-		for i := 0; i < 8; i++ {
-			for _, char := range word {
-				// fmt.Print(string(char))
-				startingIndex := getIndex(int(char))
-				fmt.Print(fileData[startingIndex+i])
+	fileData := strings.Split(string(file), "\n")
+
+	inputParts := strings.Split(input, "\\n") // Split input by "\\n" to handle newline sequence
+
+	for _, part := range inputParts {
+		if part == "" {
+			fmt.Println() // Print a newline if the part is empty (i.e., consecutive newline characters)
+			continue
+		}
+		for i := 0; i < 8; i++ { // this loop is responsible for the height of each character
+			for _, char := range part { // iterates through each character of the current word
+				startingIndex := getStartingIndex(int(char)) // obtaining the starting position of the char
+				fmt.Print(fileData[startingIndex-1+i])       // printing the character line by line
 			}
-			fmt.Println()
+			fmt.Println() // printing a new line after printing each line of the charcter
 		}
 	}
-
-	// fmt.Println(len(fileData))
 }
